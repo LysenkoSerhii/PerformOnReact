d664552789ca5fb4e3d02ca71e656b1795d3f3fc
Serhii Lysenko <lysenko.serhii.dev@gmail.com>
2023-04-21 20:24:25 +0300

    feat/shared-types

---
+++ src/shared/types/index.ts
@@ -0,0 +1,16 @@
+export interface IModels {
+	name: string;
+	field: number;
+	id: string;
+}
+
+export interface ISquareItem {
+	id: number;
+	rowsAmount: number;
+}
+
+export interface ISquare {
+	id: string;
+	colId: number;
+	rowId: number;
+}
